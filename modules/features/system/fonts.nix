dcfe9ba8404fabad992ab7b9c2cc38fa2ed1a592
nanoplank <felixargon@protonmail.com>
2026-04-23 23:35:40 +0300

    added fonts.nix

---
+++ modules/features/system/fonts.nix
@@ -0,0 +1,38 @@
+{
+  flake.nixosModules.fonts = {pkgs, ...}: {
+    fonts.packages = with pkgs; [
+      fira-code
+      fira-code-symbols
+      font-awesome
+      hackgen-nf-font
+      ibm-plex
+      inter
+      jetbrains-mono
+      maple-mono.NF
+      material-icons
+      minecraftia
+      montserrat
+      nerd-fonts.blex-mono
+      nerd-fonts.fira-mono
+      nerd-fonts.im-writing
+      nerd-fonts.iosevka-term
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.lilex
+      nerd-fonts.ubuntu
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      noto-fonts-monochrome-emoji
+      powerline-fonts
+      roboto
+      roboto-mono
+      source-sans
+      source-serif
+      symbola
+      terminus_font
+      victor-mono
+      vista-fonts
+    ];
+  };
+}
