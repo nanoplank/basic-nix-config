1c58ddde29a4627e9d0476036a9f91df679493d3
nanoplank <felixargon@protonmail.com>
2026-04-26 02:02:32 +0300

    added cursor tail to ghostty

--- modules/features/desktop/terminal/terminal.nix
+++ modules/features/desktop/terminal/terminal.nix
@@ -2,16 +2,22 @@
   flake.nixosModules.terminal = {
     pkgs,
     lib,
+    self,
     ...
   }: {
+    imports = [
+      self.nixosModules.terminalshaders
+    ];
+
     hjem.users = {
       nixed = {
         files = {
           # Ghostty
           ".config/ghostty/config.ghostty".text = "
-          theme = noctalia
-          confirm-close-surface = false
-          custom-shader = shaders/cursor_tail.glsl
+theme = noctalia
+confirm-close-surface = false
+custom-shader-animation = always
+custom-shader = shaders/cursor_tail.glsl
           ";
         };
       };
