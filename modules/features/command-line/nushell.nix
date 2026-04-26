62b91524afb414d2201e9f85401d2b4ce9d10b5f
nanoplank <felixargon@protonmail.com>
2026-04-26 18:05:33 +0300

    just testing

--- modules/features/command-line/nushell.nix
+++ modules/features/command-line/nushell.nix
@@ -13,6 +13,8 @@
           ".config/nushell/config.nu".text = "
           $env.config.show_banner = false
           $env.config.buffer_editor = 'nvim'
+          mkdir ($nu.data-dir | path join 'vendor/autoload')
+          starship init nu | save -f ($nu.data-dir | path join 'vendor/autoload/starship.nu')
           source ~/.zoxide.nu
           ";
         };
--- modules/features/desktop/terminal/terminal.nix
+++ modules/features/desktop/terminal/terminal.nix
@@ -5,11 +5,11 @@
         files = {
           # Ghostty
           ".config/ghostty/config.ghostty".text = "
-theme = noctalia
-confirm-close-surface = false
-custom-shader-animation = always
-custom-shader = shaders/cursor_tail.glsl
-shell-integration-features = ssh-env,ssh-terminfo
+          theme = noctalia
+          confirm-close-surface = false
+          custom-shader-animation = always
+          custom-shader = shaders/cursor_tail.glsl
+          shell-integration-features = ssh-env,ssh-terminfo
           ";
         };
       };
