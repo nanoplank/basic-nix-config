777dabf764ab0d34067dc4e14d10f9f2d26f5639
nanoplank <felixargon@protonmail.com>
2026-04-26 14:29:23 +0300

    ghostty ssh int

--- modules/features/desktop/terminal/terminal.nix
+++ modules/features/desktop/terminal/terminal.nix
@@ -9,6 +9,7 @@ theme = noctalia
 confirm-close-surface = false
 custom-shader-animation = always
 custom-shader = shaders/cursor_tail.glsl
+shell-integration-features = ssh-env,ssh-terminfo
           ";
         };
       };
