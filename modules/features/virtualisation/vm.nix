30f7b309c549f7833d3febdb57945eed71c26f04
nanoplank <felixargon@protonmail.com>
2026-04-26 13:13:45 +0300

    vm test for now

--- modules/features/virtualisation/vm.nix
+++ modules/features/virtualisation/vm.nix
@@ -14,16 +14,13 @@
     virtualisation = {
       libvirtd = {
         enable = true;
-        qemu = {
-          swtpm.enable = true;
-          #        ovmf.enable = true;
-          #        ovmf.packages = [ pkgs.OVMFFull.fd ];
-        };
       };
-      spiceUSBRedirection.enable = true;
     };
     services = {
       spice-vdagentd.enable = true;
+    };
+    programs = {
+      dconf.enable = true;
     };
   };
 }
