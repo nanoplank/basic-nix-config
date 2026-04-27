adbf53bc0c40f90bbd964b36c1ea67ae59717e12
nanoplank <felixargon@protonmail.com>
2026-04-27 19:04:50 +0300

    exclude some kdePackages

--- modules/features/system/services.nix
+++ modules/features/system/services.nix
@@ -1,8 +1,17 @@
-{...}: {
-  flake.nixosModules.services = {...}: {
+{pkgs, ...}: {
+  flake.nixosModules.services = {pkgs, ...}: {
     # List services that you want to enable:
     services = {
       desktopManager.plasma6.enable = true;
+    };
+    environment = {
+      plasma6 = {
+        excludePackages = [
+          pkgs.kdePackages.discover
+          pkgs.kdePackages.qrca
+          pkgs.kdePackages.konsole
+        ];
+      };
     };
   };
 }
