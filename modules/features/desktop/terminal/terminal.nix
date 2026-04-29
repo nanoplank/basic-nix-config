e35b0c909a5efdae3568c709b4a478e203e1cb7b
nanoplank <felixargon@protonmail.com>
2026-04-29 14:18:47 +0300

    made ghostty transparent + background blur

--- modules/features/desktop/terminal/terminal.nix
+++ modules/features/desktop/terminal/terminal.nix
@@ -10,6 +10,8 @@
           custom-shader-animation = always
           custom-shader = shaders/cursor_tail.glsl
           shell-integration-features = ssh-env,ssh-terminfo
+          background-opacity = 0.8
+          background-blur = true
           ";
         };
       };
