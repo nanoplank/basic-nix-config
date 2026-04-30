69af06f5d6040916f1e9bbc4d67b88bd38c9ce60
nanoplank <felixargon@protonmail.com>
2026-04-30 22:21:18 +0300

    add neo-tree

--- modules/features/command-line/nvf.nix
+++ modules/features/command-line/nvf.nix
@@ -43,16 +43,23 @@
             enable = true;
           };
 
+          filetree = {
+            neo-tree = {
+              enable = true;
+              setupOpts = {
+                enable_git_status = true;
+              };
+            };
+          };
+
           utility = {
             ccc.enable = false;
             diffview-nvim.enable = true;
-            multicursors.enable = true;
             grug-far-nvim.enable = true;
 
             motion = {
               hop.enable = true;
               leap.enable = true;
-              precognition.enable = true;
             };
           };
 
@@ -63,14 +70,40 @@
           };
 
           languages = {
-            rust.enable = true;
-            nu.enable = true;
-            nix.enable = true;
-            zig.enable = true;
-            haskell.enable = true;
-            typescript.enable = true;
-            python.enable = true;
-            lua.enable = true;
+            rust = {
+              enable = true;
+            };
+
+            nu = {
+              enable = true;
+            };
+
+            nix = {
+              enable = true;
+              format = {
+                enable = true;
+              };
+            };
+
+            zig = {
+              enable = true;
+            };
+
+            haskell = {
+              enable = true;
+            };
+
+            typescript = {
+              enable = true;
+            };
+
+            python = {
+              enable = true;
+            };
+
+            lua = {
+              enable = true;
+            };
           };
         };
       };
