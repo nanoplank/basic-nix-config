8840b746380aea86fd7983cdd468f2548ade87e3
nanoplank <felixargon@protonmail.com>
2026-05-04 15:28:38 +0300

    added an rmpc(mpd) module

--- modules/features/system/mpd.nix
+++ modules/features/system/mpd.nix
@@ -1,8 +1,13 @@
 {...}: {
-  flake.nixosModules.mpd = {...}: {
+  flake.nixosModules.mpd = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      rmpc
+      mpc
+    ];
     services = {
       mpd = {
         enable = true;
+        user = "nixed";
         settings = {
           music_directory = "/home/nixed/Music/";
           audio_output = [
