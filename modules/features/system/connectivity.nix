79295c6314816f1fbd4e3c35d878d4d4cd5ceeb4
nanoplank <felixargon@protonmail.com>
2026-05-09 15:33:28 +0300

    formatting

--- modules/features/system/connectivity.nix
+++ modules/features/system/connectivity.nix
@@ -24,7 +24,6 @@
       openssh = {
         enable = true;
         settings = {
-          UsePAM = false;
           PasswordAuthentication = false;
           KbdInteractiveAuthentication = false;
         };
