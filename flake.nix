4db417709e13c401e09908d4fc37a9cfa27e481b
nanoplank <felixargon@protonmail.com>
2026-05-17 22:21:53 +0300

    format

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
     preservation.url = "github:nix-community/preservation";
     hjem.url = "github:feel-co/hjem";
     nvf.url = "github:notashelf/nvf";
