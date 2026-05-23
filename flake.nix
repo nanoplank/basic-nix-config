5e641d7eda9441e8eda659c325db658d966399ac
nanoplank <felixargon@protonmail.com>
2026-05-24 02:46:34 +0300

    updated some inputs to follow nixpkgs in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    preservation.url = "github:nix-community/preservation";
-    hjem.url = "github:feel-co/hjem";
-    nvf.url = "github:notashelf/nvf";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    disko = {
+    url = "github:nix-community/disko";
+    inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    preservation.url = "github:nix-community/preservation"; 
+
+    sops-nix = {
+    url = "github:Mic92/sops-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+    url = "github:hercules-ci/flake-parts";
+    inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
+    hjem.url = "github:feel-co/hjem";
+
+    nvf.url = "github:notashelf/nvf"; 
   };
 
   outputs = inputs:
