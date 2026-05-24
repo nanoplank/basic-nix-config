e4f0874c4da66ff85ad3730ff39019c685944933
nanoplank <felixargon@protonmail.com>
2026-05-24 15:15:55 +0300

    hjem-rum test

--- modules/features/system/hjem.nix
+++ modules/features/system/hjem.nix
@@ -1,6 +1,9 @@
 {...}: {
-  flake.nixosModules.hjem = {...}: {
+  flake.nixosModules.hjem = {inputs, ...}: {
     hjem = {
+      extraModules = [
+        inputs.hjem-rum.hjemModules.default
+      ];
       users.nixed = {
         enable = true;
         directory = "/home/nixed";
