d0d2709e419aebcbc7331655eceede72163cf408
nanoplank <felixargon@protonmail.com>
2026-05-25 14:01:00 +0300

    $env.editor = 'nvim'

--- modules/programs/nushell.nix
+++ modules/programs/nushell.nix
@@ -10,6 +10,9 @@
                 buffer_editor = "nvim";
                 show_banner = false;
               };
+              extraConfig = "
+              $env.editor = 'nvim'
+              ";
               aliases = {
                 untar = "tar -xvf";
                 sysc = "systemctl";
