e5fe5365e0ceebd9fd1ddef7844d580c466b7e28
nanoplank <felixargon@protonmail.com>
2026-05-28 17:36:48 +0300

    update minecraft-servers module

--- modules/features/minecraft-servers.nix
+++ modules/features/minecraft-servers.nix
@@ -12,6 +12,21 @@
           vanilla = {
             enable = true;
             autoStart = false;
+            serverProperties = {
+              max-players = 4;
+              white-list = true;
+              motd = "NixOS Minecraft server!";
+            };
+            whitelist = {
+              elevons = "9d6ff915-0c04-4e95-b1b2-0b1dc3b47c88";
+            };
+            operators = {
+              elevons = {
+                uuid = "9d6ff915-0c04-4e95-b1b2-0b1dc3b47c88";
+                level = 3;
+                bypassesPlayerLimit = true;
+              };
+            };
             jvmOpts = "-Xmx4G -Xms4G";
           };
         };
