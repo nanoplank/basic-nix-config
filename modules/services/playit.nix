fd68a13a3adc052fd87fa64edbd43752cd605f69
nanoplank <felixargon@protonmail.com>
2026-06-01 14:02:10 +0300

    disable playit service autostart on boot

--- modules/services/playit.nix
+++ modules/services/playit.nix
@@ -1,11 +1,17 @@
 {inputs, ...}: {
-  flake.nixosModules.playit = {config, ...}: {
+  flake.nixosModules.playit = {
+    config,
+    lib,
+    ...
+  }: {
     imports = [inputs.playit-nixos-module.nixosModules.default];
     sops.secrets.playit-secret = {};
 
+    systemd.services.playit.wantedBy = lib.mkForce [];
+
     services = {
       playit = {
-        enable = false;
+        enable = true;
         secretPath = config.sops.secrets.playit-secret.path;
       };
     };
