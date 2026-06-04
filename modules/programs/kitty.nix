693ed036659a4ba8f590fe022a502a71df0314d0
nanoplank <felixargon@protonmail.com>
2026-06-04 20:16:38 +0300

    change background opacity to 1

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -10,7 +10,7 @@
                 include = "themes/noctalia.conf";
                 font_family = "JetBrainsMono";
                 confirm_os_window_close = 0;
-                background_opacity = 0.7;
+                background_opacity = 1;
                 cursor_trail = 1;
                 font_size = 10;
               };
