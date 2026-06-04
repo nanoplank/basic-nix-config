d8ae45ed569cca00aeffbdb967daecd9c8e120f0
nanoplank <felixargon@protonmail.com>
2026-06-04 20:14:06 +0300

    add a hjem-rum helix module

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -7,18 +7,7 @@
             helix = {
               enable = true;
               settings = {
-                theme = "onedark";
-                editor = {
-                  editor = {
-                    statusline = {
-                      left = [
-                        "mode"
-                        "spinner"
-                      ];
-                    };
-                  };
-                  line-number = "relative";
-                };
+                theme = "noctalia";
               };
             };
           };
