52e6ffbcb98904f7485786202b2843014e990476
nanoplank <felixargon@protonmail.com>
2026-06-05 03:04:58 +0300

    formatting + removed direnv

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,17 +1,16 @@
 {...}: {
   flake.nixosModules.nix = {pkgs, ...}: {
-    programs.direnv = {
-      enable = true;
-      silent = false;
-      loadInNixShell = true;
-      direnvrcExtra = "";
-      nix-direnv = {
-        enable = true;
+    nix = {
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
       };
     };
 
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       nix-inspect
