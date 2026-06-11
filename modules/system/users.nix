22c87e3e8f84f6a65c3d1ec57465a93d01c5508d
nanoplank <felixargon@protonmail.com>
2026-06-11 21:13:59 +0300

    manage authorized_keys using sops

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -4,6 +4,8 @@
     pkgs,
     ...
   }: {
+    sops.secrets."users/nixed/password".neededForUsers = true;
+    users.mutableUsers = false;
     users.users.nixed = {
       isNormalUser = true;
       hashedPasswordFile = config.sops.secrets."users/nixed/password".path;
@@ -11,7 +13,13 @@
       extraGroups = ["networkmanager" "wheel" "libvirtd"];
       shell = pkgs.nushell;
     };
-    sops.secrets."users/nixed/password".neededForUsers = true;
-    users.mutableUsers = false;
+    sops.secrets."publickeys/key" = {};
+    sops.templates."authorized_keys" = {
+      owner = "nixed";
+      content = ''
+      ${config.sops.placeholder."publickeys/key"}
+      '';
+      path = "/home/nixed/.ssh/authorized_keys";
+    };
   };
 }
