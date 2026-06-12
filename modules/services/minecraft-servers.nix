f72dd7acd8f9c28502cc593ae9b680bf67030f7f
nanoplank <felixargon@protonmail.com>
2026-06-13 02:26:22 +0300

    minor minecraft-server module changes (added offline 1.16.5 server)

--- modules/services/minecraft-servers.nix
+++ modules/services/minecraft-servers.nix
@@ -9,12 +9,13 @@
         eula = true;
         openFirewall = true;
         servers = {
+          # latest (online-mode)
           vanilla = {
             enable = true;
             autoStart = false;
             jvmOpts = "-Xmx4G -Xms4G";
 
-            package = pkgs.vanillaServers.vanilla-26_1_2;
+            package = pkgs.vanillaServers.vanilla;
 
             serverProperties = {
               max-players = 4;
@@ -29,6 +30,28 @@
               };
             };
           };
+          #   # 1.16.5 (offline-mode)
+          #   vanilla-offline = {
+          #     enable = true;
+          #     autoStart = false;
+          #     jvmOpts = "-Xmx4G -Xms4G";
+
+          #     package = pkgs.vanillaServers.vanilla-1_16_5;
+
+          #     serverProperties = {
+          #       max-players = 4;
+          #       online-mode = false;
+          #       motd = "NixOS Minecraft server!";
+          #     };
+
+          #     operators = {
+          #       elevons = {
+          #         uuid = "cf3e79eb-84a3-39d5-9148-ea8a9af1fc6b";
+          #         level = 3;
+          #         bypassesPlayerLimit = true;
+          #       };
+          #     };
+          #   };
         };
       };
     };
