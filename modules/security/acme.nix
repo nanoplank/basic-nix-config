581b5a77aac433e4b4f8847c0bc43469bfc209bc
nanoplank <felixargon@protonmail.com>
2026-06-13 02:14:32 +0300

    added acme module (for XMPP)

---
+++ modules/security/acme.nix
@@ -0,0 +1,22 @@
+{...}: let
+  domain = "xmpp.stuff.com";
+  mucDomain = "conference.${domain}";
+  uploadDomain = "upload.${domain}";
+in {
+  flake.nixosModules.acme = {...}: {
+    security = {
+      acme = {
+        acceptTerms = true;
+        defaults = {
+          email = "stuff@foobar.com";
+          certs.${domain} = {
+            group = "certs";
+            webroot = "/var/lib/acme/acme-challenge";
+            postRun = "systemctl reload prosody.service";
+            extraDomainNames = [mucDomain uploadDomain];
+          };
+        };
+      };
+    };
+  };
+}
