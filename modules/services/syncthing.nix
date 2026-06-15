bf644bb87a1e8ae53a9ee983fc87f4c39d63a537
nanoplank <felixargon@protonmail.com>
2026-06-15 03:24:03 +0300

    manage syncthing guipassword using sops

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,8 +1,21 @@
 {...}: {
-  flake.nixosModules.syncthing = {...}: {
+  flake.nixosModules.syncthing = {config, ...}: {
+    sops.secrets."syncthing/guipassword" = {};
     services = {
       syncthing = {
         enable = true;
+        guiPasswordFile = config.sops.secrets."syncthing/guipassword".path;
+        settings = {
+          options = {
+            urAccepted = -1;
+            user = "nixed";
+          };
+          folders = {
+            "/var/lib/syncthing" = {
+              id = "shared";
+            };
+          };
+        };
       };
     };
   };
