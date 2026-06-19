a02d6474ecbe372b436c5347f8ce6659828d398c
nanoplank <felixargon@protonmail.com>
2026-06-19 12:57:41 +0300

    remove unnecessary comments | formatting

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,23 +1,20 @@
 {...}: {
   flake.nixosModules.steam = {pkgs, ...}: {
     programs = {
-      gamemode.enable = true;
-      gamescope.enable = true;
+      gamemode = {
+        enable = true;
+      };
+      gamescope = {
+        enable = true;
+      };
       steam = {
         enable = true;
-        #    package = pkgs.steam.override {
-        #       extraProfile = ''
-        #         unset TZ
-        #         export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
-        #      '';
-
+        protontricks = {
+          enable = true;
+        };
         extraCompatPackages = with pkgs; [
           proton-ge-bin
         ];
-        # extraPackages = with pkgs; [
-        #   SDL2
-        # ];
-        protontricks.enable = true;
       };
     };
   };
