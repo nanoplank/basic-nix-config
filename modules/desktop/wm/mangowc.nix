7acd980c374bf7165b134ce6875f82cd065137a7
nanoplank <felixargon@protonmail.com>
2026-06-20 20:12:32 +0300

    updated the mangowc module

--- modules/desktop/wm/mangowc.nix
+++ modules/desktop/wm/mangowc.nix
@@ -28,6 +28,98 @@
       autostart_sh = ''
         ${lib.getExe self'.packages.noctaliawrap}
       '';
+
+      extraConfig = ''
+        source=/home/nixed/.config/mango/noctalia.conf
+      '';
+
+      settings = {
+        repeat_rate = 40;
+        repeat_delay = 250;
+        numlockon = 0;
+        xkb_rules_layout = "us,ru";
+        xkb_rules_options = "caps:swapescape";
+        trackpad_natural_scrolling = 1;
+        enable_hotarea = 0;
+
+        # layout settings
+        circle_layout = "scroller,dwindle";
+
+        # scroller
+        scroller_default_proportion = 0.5;
+        edge_scroller_pointer_focus = 0;
+        scroller_structs = 20;
+
+        # dwindle
+        # dwindle_split_ratio = 0.5;
+        # dwindle_smart_split = 1;
+
+        tagrule = [
+          "id:1,layout_name:scroller"
+          "id:2,layout_name:scroller"
+          "id:3,layout_name:scroller"
+          "id:4,layout_name:scroller"
+          "id:5,layout_name:scroller"
+          "id:6,layout_name:scroller"
+          "id:7,layout_name:scroller"
+          "id:8,layout_name:scroller"
+          "id:9,layout_name:scroller"
+          "id:10,layout_name:scroller"
+        ];
+
+        bind = let
+          mod = "SUPER";
+        in [
+          "${mod},r,spawn,${lib.getExe pkgs.fuzzel} -I -f 'JetBrains Mono' --hide-before-typing --lines 1"
+          "${mod},q,spawn,kitty"
+
+          "${mod},c,killclient"
+
+          "${mod},Left,focusdir,left"
+          "${mod},Right,focusdir,right"
+          "${mod},Up,focusdir,up"
+          "${mod},Down,focusdir,down"
+
+          "${mod}+SHIFT,Up,exchange_client,up"
+          "${mod}+SHIFT,Down,exchange_client,down"
+          "${mod}+SHIFT,Left,exchange_client,left"
+          "${mod}+SHIFT,Right,exchange_client,right"
+
+          "${mod},f,togglemaximizescreen"
+          "${mod}+shift,f,togglefloating"
+          "${mod},g,togglefullscreen"
+
+          "SUPER,n,switch_layout"
+
+          "${mod},1,view,1,0"
+          "${mod},2,view,2,0"
+          "${mod},3,view,3,0"
+          "${mod},4,view,4,0"
+          "${mod},5,view,5,0"
+          "${mod},6,view,6,0"
+          "${mod},8,view,7,0"
+          "${mod},9,view,8,0"
+          "${mod},0,view,9,0"
+
+          "${mod}+SHIFT,1,tag,1,0"
+          "${mod}+SHIFT,2,tag,2,0"
+          "${mod}+SHIFT,3,tag,3,0"
+          "${mod}+SHIFT,4,tag,4,0"
+          "${mod}+SHIFT,5,tag,5,0"
+          "${mod}+SHIFT,6,tag,6,0"
+          "${mod}+SHIFT,8,tag,7,0"
+          "${mod}+SHIFT,9,tag,8,0"
+          "${mod}+SHIFT,0,tag,9,0"
+
+          "NONE,XF86MonBrightnessUp,spawn,${lib.getExe pkgs.brightnessctl} s 2%+"
+          "NONE,XF86MonBrightnessDown,spawn,${lib.getExe pkgs.brightnessctl} s 2%-"
+
+          "NONE,XF86AudioRaiseVolume,spawn,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          "NONE,XF86AudioLowerVolume,spawn,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+
+          "NONE,XF86AudioMute,spawn,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ];
+      };
     };
   };
 }
