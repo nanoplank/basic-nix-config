171743d30887fb9190bf7dfe38e2adae0f9557a4
nanoplank <felixargon@protonmail.com>
2026-06-25 21:42:05 +0300

    move: modules/system/preservation.nix -> modules/preservation/preservation.nix

---
+++ modules/preservation/preservation.nix
@@ -0,0 +1,65 @@
+{inputs, ...}: {
+  flake.nixosModules.preservation = {...}: {
+    imports = [inputs.preservation.nixosModules.default];
+
+    preservation = {
+      enable = true;
+      preserveAt."/persistent" = {
+        directories = [
+          "/var/lib/power-profiles-daemon"
+          "/var/lib/systemd/timers"
+          "/var/lib/bluetooth"
+          "/var/lib/tailscale"
+          "/var/lib/libvirt"
+          "/srv/minecraft"
+          "/var/lib/mpd"
+          {
+            directory = "/var/lib/nixos";
+            inInitrd = true;
+          }
+        ];
+
+        files = [
+          {
+            file = "/etc/machine-id";
+            inInitrd = true;
+          }
+        ];
+
+        users = {
+          nixed = {
+            directories = [
+              "basic-nix-config"
+              "Wallpapers"
+              "Documents"
+              "Projects"
+              "Music"
+
+              # dotfiles
+              ".mozilla"
+              ".steam"
+              ".ssh"
+
+              # .config
+              ".config/noctalia"
+              ".config/qt6ct"
+              ".config/librewolf"
+              ".config/r2modman"
+              ".config/r2modmanPlus-local"
+              ".config/sops"
+
+              # .cache
+              ".cache/noctalia"
+
+              # .local/share
+              ".local/share/zoxide"
+              ".local/share/PrismLauncher"
+              ".local/share/osu"
+              ".local/share/Steam"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
