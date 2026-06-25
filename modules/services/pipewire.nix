265332c3f1711f8290a8d4a56899fcbca6f50f07
nanoplank <felixargon@protonmail.com>
2026-06-25 21:37:25 +0300

    add: pipewire module

---
+++ modules/services/pipewire.nix
@@ -0,0 +1,29 @@
+{...}: {
+  flake.nixosModules.pipewire = {...}: {
+    services = {
+      pipewire = {
+        enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+
+        audio = {
+          enable = true;
+        };
+
+        pulse = {
+          enable = true;
+        };
+
+        jack = {
+          enable = true;
+        };
+
+        wireplumber = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
