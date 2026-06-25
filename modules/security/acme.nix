923480087307d84076483a9e3baacd63edd2713f
nanoplank <felixargon@protonmail.com>
2026-06-25 21:46:11 +0300

    formatting

--- modules/security/acme.nix
+++ modules/security/acme.nix
@@ -9,11 +9,13 @@ in {
         acceptTerms = true;
         defaults = {
           email = "stuff@foobar.com";
-          certs.${domain} = {
-            group = "certs";
-            webroot = "/var/lib/acme/acme-challenge";
-            postRun = "systemctl reload prosody.service";
-            extraDomainNames = [mucDomain uploadDomain];
+          certs = {
+            ${domain} = {
+              group = "certs";
+              webroot = "/var/lib/acme/acme-challenge";
+              postRun = "systemctl reload prosody.service";
+              extraDomainNames = [mucDomain uploadDomain];
+            };
           };
         };
       };
